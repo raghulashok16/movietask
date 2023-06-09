2d7a81b7385e88de3cd84564d7b412a96acfe410
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2023-06-09 21:10:20 +0530

    installed infinite scroll component

--- src/components/moviecards.jsx
+++ src/components/moviecards.jsx
@@ -1,15 +1,21 @@
 import Card from "./card";
-const MovieCards = ({ movies }) => {
+import InfiniteScroll from "react-infinite-scroll-component";
+const MovieCards = ({ movies, setMovies, page, setPage }) => {
     // console.log(movies);
     // console.log(props.MovieCard);
+    // console.log(movies.length);
+    const loadMore = () => {
+        console.log("load more");
+    }
     return (
         <>
+            {/* <InfiniteScroll dataLength={movies.length} next={loadMore} className=""> */}
             {
                 movies.map((movie) => (
                     <Card key={movie.imdbID} movie={movie} />
                 ))
             }
-
+            {/* </InfiniteScroll> */}
         </>
     )
 };
--- src/routes/list.jsx
+++ src/routes/list.jsx
@@ -6,24 +6,25 @@ const List = () => {
     const [movies, setMovies] = useState([]);
     const [searchText, setSearchText] = useState('');
     const [total, setTotal] = useState('0');
+    const [page, setPage] = useState('1')
 
-    const apiCall = async (searchText) => {
-        const url = `http://www.omdbapi.com/?s=${searchText}&apikey=722ecd59`;
+    const apiCall = async (searchText, page) => {
+        const url = `http://www.omdbapi.com/?s=${searchText}&apikey=722ecd59&page=${page}`;
         const response = await fetch(url);
         const result = await response.json();
         if (result.Search) {
             setMovies(result.Search);
             setTotal(result.totalResults);
         }
-        if (result.Error == "Too many results." || result.Error == "Movie not found!") {
+        if (result.Error === "Too many results." || result.Error === "Movie not found!") {
             setMovies([]);
             setTotal(result.Error);
         }
 
     }
     useEffect(() => {
-        apiCall(searchText);
-        if (searchText == "") {
+        apiCall(searchText, page);
+        if (searchText === "") {
             setMovies([]);
         }
     }, [searchText])
@@ -37,7 +38,7 @@ const List = () => {
     return (
         <>
             {
-                (searchText != "") ? (<p className="badge fs-5 ms-3 mt-3 text-bg-dark">total result found = {total}</p>) : (<p className="badge fs-5 ms-3 mt-3 text-bg-dark">Enter movie name to search</p>)
+                (searchText !== "") ? ((total === 'Movie not found!') ? (<p className="badge fs-5 ms-3 mt-3 text-bg-dark">Movie not found!</p>) : (<p className="badge fs-5 ms-3 mt-3 text-bg-dark">total result found = {total}</p>)) : (<p className="badge fs-5 ms-3 mt-3 text-bg-dark">Enter movie name to search</p>)
             }
             <div className="container mt-0 mb-4">
                 <SearchBox
@@ -45,8 +46,9 @@ const List = () => {
                     onChangeHandler={onChangeEvent}
                 />
                 <div className="row justify-content-evenly">
+
                     <MovieCards
-                        movies={movies}
+                        movies={movies} setMovies={setMovies} page={page} setPage={setPage}
                     />
                 </div>
             </div>
