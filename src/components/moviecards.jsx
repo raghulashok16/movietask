e471ceecea9307b14f66cfd3f71d0a01e06c8071
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2023-06-12 20:17:29 +0530

    reverted 2

--- src/components/moviecards.jsx
+++ src/components/moviecards.jsx
@@ -1,34 +1,30 @@
-import { useEffect, useState } from "react";
 import Card from "./card";
 import InfiniteScroll from "react-infinite-scroll-component";
 const MovieCards = ({ movies, page, setPage, total }) => {
-    const [fetchWork, setFetchWork] = useState(false)
-    const [has, setHas] = useState(true)
-    // console.log(fetchWork, movies, page);
-    // useEffect(() => {
-    //     if (page >= 7) {
-    //         setHas(false);
-    //     }
-    // }, [page])
+
+
 
     return (
         <>
             {movies.length === 0 || (
                 <InfiniteScroll dataLength={total}
                     next={() => {
-                        if (fetchWork === false) {
-                            setPage(page + 1)
-                        }
+                        // console.log("load more");
+                        // console.log(total);
+                        setPage(page + 1)
                     }}
-                    hasMore={has}
+                    hasMore={true}
+                    className="row justify-content-evenly"
                     loader={<p className="w-100 text-center">Loading...</p>}
-                    className="d-flex flex-wrap justify-content-evenly"
                     endMessage={
                         <p className="text-center w-100">
                             <b>End of List</b>
                         </p>
                     }
                 >
+                    {
+                        console.log(movies)
+                    }
                     {
                         movies.map((movie) => (
                             <Card key={movie.imdbID} movie={movie} />
